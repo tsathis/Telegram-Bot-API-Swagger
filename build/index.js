4f39052d875470811a775d28a1f36799b1bb345f
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2021-03-18 11:37:40 +0530

    fix(ui): Add css

--- build/index.js
+++ build/index.js
@@ -4,12 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const css_1 = require("./css");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require(`${__dirname}/../swagger/JosXa-telegram-bot_api-5.0.0-swagger.json`);
 console.log("Started!");
 const app = express_1.default();
 app.get("/ping", (req, res) => { res.json({ "Status": "OK" }); });
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+let options = {
+    customCss: css_1.css
+};
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 const PORT_NUMBER = 3000;
 app.listen(process.env.PORT || PORT_NUMBER, function () {
     console.log(`Listening on https://localhost:${process.env.PORT || PORT_NUMBER}`);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { css } from './css';
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require(`${__dirname}/../swagger/JosXa-telegram-bot_api-5.0.0-swagger.json`);
@@ -9,7 +10,10 @@ const app = express();
 
 app.get("/ping", (req: Request, res: Response) => { res.json({ "Status": "OK" }); });
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+let options = {
+  customCss: css
+};
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 const PORT_NUMBER = 3000;
 app.listen(process.env.PORT || PORT_NUMBER, function () {
